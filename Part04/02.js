e98a24d2761e9bfa2e000d7af30f57237f559ff0
nobrain711 <ubuntu@ip-172-31-18-46.ap-northeast-2.compute.internal>
2022-04-22 16:49:27 +0000

    22/04/23

---
+++ Part04/02.js
@@ -0,0 +1,27 @@
+//04_05
+console.log("score1 :" + score1); //결과 undefined
+
+var score1; //JS는 순차적인 언어여서 undefined을 출력함
+
+//04_06
+var score2;
+score2 = 80;
+console.log("score2 : " + score2);
+
+//04_07
+var score3 = 80;
+console.log("score3 : " + score3);
+// 04_06번 하고 04_07번 하고는 같은 값이나 04_07방식이 주로 사용 된다
+
+//04_08
+var score4;
+console.log("값 입력 전 score4 : " + score4);
+
+var score4 = 80;
+console.log("값 입력 후 score4 : " + score4);
+
+//04_09
+score5 = 80; //score5에 80을 저장
+var score5; //메모리 공간에 score5를 올려준다
+
+console.log("score5 : " + score5);
---
+++ Part04/03.js
@@ -0,0 +1,12 @@
+var Vscore = 80;
+Vscore = 90;
+
+console.log(Vscore);
+
+const Csocre = 10;
+Csocre = 100;
+
+/**var와 const의 차이
+ * var는 지역변수를 선언해 주고
+ * const는 전역변수를 사용하게 해준다
+ */
